07f5467dcc2db522c5f69a2a32b64934d4433a8f
dim web <dmitrymail_k@mail.ru>
2022-03-11 13:58:52 +0300

    add some

---
+++ openMP/rip_tutorial/getting_started_mp/README.md
@@ -0,0 +1,3 @@
+## Simple program
+
+- [getting started with openmp](https://riptutorial.com/openmp)
---
+++ openMP/volsu_tasks/matrix_vector_multiplication/matrix_vector_multiplication.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <omp.h>
+
+int main(int argc, const char **argv)
+{
+	return 0;
+}
