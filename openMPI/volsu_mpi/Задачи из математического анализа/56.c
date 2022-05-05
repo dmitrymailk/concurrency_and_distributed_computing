1a44ab9c826061f62a79bd4bdbb35da72bd5498e
dmitrymailk <dmitrymail_k@mail.ru>
2022-05-05 21:59:31 +0300

    solve 56 mpi

--- openMPI/volsu_mpi/Задачи из математического анализа/56.c
+++ openMPI/volsu_mpi/Задачи из математического анализа/56.c
@@ -16,33 +16,41 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
-	int N = 1000;
+	int chunk = 10000;
 
     if (ProcRank == 0)
     {
         // Действия, выполняемые только процессом с рангом 0
+		long double all_pi = 0.0;
+
         for (int i = 1; i < ProcNum; i++)
         {
-            // получение сообщение из любого ресурса
-            // MPI_Recv(&RecvRank, 1, MPI_INT, MPI_ANY_SOURCE,
-            //          MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-
+            long double process_pi;
             // получение сообщения из ресурса под номером i
-            MPI_Recv(&RecvRank, 1, MPI_INT, i,
+            MPI_Recv(&process_pi, 1, MPI_LONG_DOUBLE, i,
                      MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
 
-            printf("Hello from process %3d\n", RecvRank);
+			all_pi += process_pi;
         }
+		
+		printf("Computed pi^2/6 = %Lf\n", all_pi);
     }
     else
-    {
-        // Сообщение, отправляемое всеми процессами,
-        // кроме процесса с рангом 0
-        MPI_Send(&ProcRank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    {	
+		int iter = ProcRank - 1;
+		int start = 1 + chunk * iter;
+		int end = 1 + chunk * (iter+1);
+
+		long double result = 0.0;
+
+		for(int n=start; n < end; n++)
+		{
+			result += (long double)(1 / (long double)(n*n)); 
+		}
+
+        MPI_Send(&result, 1, MPI_LONG_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
-    // double time_mpi_end = MPI_Wtime();
-    // printf("Working time is %f on host %d\n", time_mpi_end - time_mpi_start, ProcRank);
     return 0;
 }
